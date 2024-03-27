af81f615dd48a0704d6c6b7d4d0bd413d0423db5
Ditha Raju <ditharaju91@yahoo.com>
2024-03-27 22:13:55 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,component,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e0ffff", // Light blue
+        secondary: "#f0fff0", // Light green
+        accent: "#ffc0cb", // Light pink
+      },
+    },
+  },
+  plugins: [],
+};
